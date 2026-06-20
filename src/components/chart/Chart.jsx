25744fd423588118d218ea2902f3ded5b6f1fab4
vamppdevv1 <pacounte@gmail.com>
2026-06-20 21:13:56 +0100

    feat: add dashboard charts

--- src/components/chart/Chart.jsx
+++ src/components/chart/Chart.jsx
@@ -1,7 +1,48 @@
 import "./chart.scss"
-
+import {
+  ResponsiveContainer,
+  AreaChart,
+  ReferenceLine,
+  Area,
+  XAxis,
+  CartesianGrid,
+  Tooltip,
+} from "recharts";
+const data = [
+  { name: "January", Total: 1200 },
+  { name: "February", Total: 2100 },
+  { name: "March", Total: 800 },
+  { name: "April", Total: 1600 },
+  { name: "May", Total: 900 },
+  { name: "June", Total: 1700 },
+];
 export const Chart = () => {
   return (
-    <div>Chart</div>
-  )
+    <div className="chart">
+      <div className="title">Last 6 Months (Revenue)</div>
+      <ResponsiveContainer width="100%" aspect={1.6} maxHeight={500}>
+        <AreaChart
+          data={data}
+          margin={{ top: 10, right: 30, left: 0, bottom: 0 }}
+        >
+          <XAxis dataKey="name" />
+          <CartesianGrid strokeDasharray="3 3" />
+          <Tooltip />
+          <ReferenceLine x="Page C" stroke="green" label="Min PAGE" />
+          <ReferenceLine
+            y={4000}
+            label="Max"
+            stroke="red"
+            strokeDasharray="3 3"
+          />
+          <Area
+            type="monotone"
+            dataKey="Total"
+            stroke="#8884d8"
+            fill="#8884d8"
+          />
+        </AreaChart>
+      </ResponsiveContainer>
+    </div>
+  );
 }
