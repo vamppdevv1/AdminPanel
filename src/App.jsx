5f17f887b69cb82b91f61746ab4b40b57899f7c6
vamppdevv1 <pacounte@gmail.com>
2026-07-01 06:30:17 +0100

    adding darkmode

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,14 @@ import { List } from "./pages/list/List";
 import { Single } from "./pages/single/Single";
 import { New } from "./pages/new/New";
 import { userInputs, productInputs } from "./pages/new/formSource";
+import "./style/dark.scss";
+import { DarkModeContext } from "./context/darkModeContext.jsx";
+import { useContext } from "react";
 function App() {
+const {darkMode} = useContext(DarkModeContext)
+
   return (
-    <div className="App">
+    <div className={darkMode ? "app dark" : 'app'}>
       <BrowserRouter>
         <Routes>
           <Route path="/">
@@ -16,12 +21,18 @@ function App() {
             <Route path="users">
               <Route index element={<List />} />
               <Route path=":userId" element={<Single />} />
-              <Route path="new" element={<New inputs = {userInputs} title="Add New User" />} />
+              <Route
+                path="new"
+                element={<New inputs={userInputs} title="Add New User" />}
+              />
             </Route>
             <Route path="products">
               <Route index element={<List />} />
               <Route path=":productId" element={<Single />} />
-              <Route path="new" element={<New inputs={productInputs} title="Add New Product"/>} />
+              <Route
+                path="new"
+                element={<New inputs={productInputs} title="Add New Product" />}
+              />
             </Route>
           </Route>
         </Routes>
